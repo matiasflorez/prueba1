3fa1fce7d3f07cfdd5d393f8625302b61686a2cb
matiasflorez <a20170045@juan23.edu.pe>
2024-10-29 20:35:13 -0500

    Add files via upload

---
+++ app01.js
@@ -0,0 +1,5 @@
+document.write("<h1>Bienvenido a la pagina de JavaScript</h1>");
+var nom = prompt("Ingresar su Nombre");
+document.write(
+  "Hola " + nom + " es tu primer contacto con la programación JavaScript"
+);
